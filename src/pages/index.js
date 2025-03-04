c6e6eccc816e826d668bce6f8ec8d7e12127ca8b
NOE BALADAD <NOEBALADAD@GMAIL.COM>
2025-03-04 11:17:10 -1000

    Sprint 9 project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
+import "./index.css";
+
+import { enableValidation, settings } from "../scripts/validation.js";
+
+import spotsSrc from "./images/spots_logo.svg"
+
+const spotsLogo = document.getElementById("spots-logo");
+spotsLogo.src = spotsSrc;
+
 const initialCards = [
   {name: "Val Thorens", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"},
   {name: "Restaurant terrace", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"},
@@ -139,3 +148,4 @@ initialCards.forEach((item) => {
   console.log(item)
 });
 
+enableValidation(settings);
--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,4 +1,4 @@
-const settings = {
+export const settings = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__submit-button",
@@ -69,11 +69,10 @@ const setEventListeners = (formElement, config) => {
   });
 }
 
-const enableValidation = (config) => {
+export const enableValidation = (config) => {
   const formList = document.querySelectorAll(config.formSelector);
   formList.forEach((formElement) => {
     setEventListeners(formElement, config);
   });
 };
 
-enableValidation(settings);
